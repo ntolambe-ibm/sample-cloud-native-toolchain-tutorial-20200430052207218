142c3b25c89de5a39d06637256faf72e978c122e
Nikhil Tolambe <ntolambe@in.ibm.com>
2020-04-30 16:23:11 +0000

    Hello There

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hey Hello, stranger, How are you doing!');
+            res.body.should.eql('Hello there, Scott!');
 
             done();
           });
