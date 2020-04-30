96b5b24c3b8a460e0656b63acda2b26fec9346d8
Nikhil Tolambe <ntolambe@in.ibm.com>
2020-04-30 14:25:16 +0000

    changed the display message "Hello"

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, stranger!');
+            res.body.should.eql('Hey Hello, stranger, How are you doing!');
 
             done();
           });
